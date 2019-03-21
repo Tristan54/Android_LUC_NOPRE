016bdaa37d80916e5d09032fa09481e6b192f15b
luc21u <tristan.luc1@etu-univ.lorraine.fr>
2019-03-21 10:08:09 +0100

    en cour

---
+++ app/src/main/java/fr/luc_nopre/projet/Receiver.java
@@ -0,0 +1,17 @@
+package fr.luc_nopre.projet;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class Receiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Alarm Triggered", Toast.LENGTH_SHORT).show();
+
+    }
+
+}
